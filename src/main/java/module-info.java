bc8a783f53377d56cccea31532b71ad2bd19899a
Radek Kulig <radek.kulig@op.pl>
2020-08-05 10:11:47 +0200

    Add mainPane.fxml which is responsible for look of the application

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,7 @@
 module mp3player {
     requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
 
     exports pl.rkulig.mp3player.main to javafx.graphics;
 }
--- src/main/java/pl/rkulig/mp3player/main/Main.java
+++ src/main/java/pl/rkulig/mp3player/main/Main.java
@@ -1,6 +1,9 @@
 package pl.rkulig.mp3player.main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,6 +14,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        Pane mainPane = FXMLLoader.load(getClass().getResource("/fxml/mainPane.fxml"));
+        Scene scene = new Scene(mainPane);
+        stage.setScene(scene);
         stage.setTitle("Mp3 Player");
         stage.show();
     }
