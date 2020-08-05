ef6e89dc1dd18b25fedea9740cdb123907b6bfac
Radek Kulig <radek.kulig@op.pl>
2020-08-05 09:47:03 +0200

    Add launching method

---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module mp3player {
+    requires javafx.graphics;
+
+    exports pl.rkulig.mp3player.main to javafx.graphics;
+}
---
+++ src/main/java/pl/rkulig/mp3player/main/Main.java
@@ -0,0 +1,17 @@
+package pl.rkulig.mp3player.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Mp3 Player");
+        stage.show();
+    }
+}
