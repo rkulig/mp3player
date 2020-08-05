b20fa60c67f69531e0336805e9fdd4ad0e344092
Radek Kulig <radek.kulig@op.pl>
2020-08-05 10:53:59 +0200

    Add methods responsible for button and slider actions

--- src/main/java/pl/rkulig/mp3player/controller/MainController.java
+++ src/main/java/pl/rkulig/mp3player/controller/MainController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TableView;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.input.MouseEvent;
 
 public class MainController {
 
@@ -40,6 +41,25 @@ public class MainController {
     private Slider progressSlider;
 
     public void initialize() {
-        System.out.println("MainController created");
+        configureButtons();
+        configureVolume();
+    }
+
+    private void configureVolume() {
+        volumeSlider.addEventFilter(MouseEvent.MOUSE_PRESSED, event ->
+                System.out.println("Wciśnięto przycisk na suwaku głośności")
+        );
+    }
+
+    private void configureButtons() {
+        previousButton.setOnAction(event -> System.out.println("Poprzednia piosenka"));
+        nextButton.setOnAction(x -> System.out.println("Następna piosenka"));
+        playButton.setOnAction(event -> {
+            if(playButton.isSelected()) {
+                System.out.println("Play");
+            } else {
+                System.out.println("Stop");
+            }
+        });
     }
 }
